589b84994e0d407bba7bd25f3ef80e61b49f16eb
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-10-22 23:00:10 +0900

    add data

--- collection/lib1/00_batch.sh
+++ collection/lib1/00_batch.sh
@@ -1,5 +1,5 @@
 # python 01_getImageJson.py
 python 02_createMediaList.py 
 python 03_createThumbList.py
-python 04_createTocList.py
-python 05_createTextList.py
+# python 04_createTocList.py
+# python 05_createTextList.py
